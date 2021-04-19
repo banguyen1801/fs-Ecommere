fcb562e307dbe2b8a356a3ed74d2960be7c371b6
Ba Nguyen <b.pynguyen@gmail.com>
2021-04-19 18:32:32 +0700

    delete commented code in models

--- src/models/Products.js
+++ src/models/Products.js
@@ -17,33 +17,3 @@ const productSchema = new mongoose.Schema({
 });
 
 export default mongoose.model('Product', productSchema);
-// class ProductClass {
-//   constructor({ name, categories }) {
-//     console.log('name, categories', name, categories);
-//     this.model = new Product();
-//     this.model.name = name;
-//     this.model.categories = categories;
-//   }
-
-//   async save() {
-//     try {
-//       var savedProduct = await this.model.save();
-//     } catch (err) {
-//       console.log(err);
-//     }
-//     return savedProduct;
-//   }
-
-//   static async findProductByName(name) {
-//     const result = await Product.findOne({ name: name });
-//     console.log(result);
-//     return result;
-//   }
-//   static async findAllProduct() {
-//     const result = await Product.find().exec();
-//     console.log(result);
-//     return result;
-//   }
-// }
-
-// export default ProductClass;
--- src/models/Roles.js
+++ src/models/Roles.js
@@ -8,30 +8,3 @@ const roleSchema = new Schema({
   },
 });
 export default mongoose.model('Role', roleSchema);
-
-// class RoleClass {
-//   constructor(name) {
-//     this.model = new roleSchema();
-//     this.model.name = name;
-//   }
-//   async save() {
-//     try {
-//       var savedRole = await this.model.save();
-//     } catch (err) {
-//       console.log(err);
-//     }
-//     return savedRole;
-//   }
-
-//   static async getUserId() {
-//     const role = await Role.findOne({ name: 'user' });
-//     return role._id;
-//   }
-
-//   static async getAdminId() {
-//     const role = Role.findOne({ name: 'admin' });
-//     return role._id;
-//   }
-// }
-
-// export default RoleClass;
